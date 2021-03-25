07cc7ac4ef71c16735348b5f86da91785d6baf3a
Olivier Tremblay-Boulet <melonspeedruns@outlook.com>
2021-03-25 12:05:24 -0400

    Initial Commit 2!

--- OoT3D-Rando-Launcher/Form1.cs
+++ OoT3D-Rando-Launcher/Form1.cs
@@ -1,21 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace OoT3D_Rando_Launcher
 {
     public partial class Form1 : Form
     {
+        private string emuPath;
+        private string romPath;
+        private string randomizerRomPath;
+
+        private string filesPath = Path.Combine("user", "sdmc", "luma", "titles", "0004000000033500");
+        private string modPath = Path.Combine("user", "load", "mods", "0004000000033500");
+
         public Form1()
         {
             InitializeComponent();
+            RestoreSavedValues();
         }
 
+        public void SaveValues()
+        {
+            Properties.Settings.Default.emuPath = textBox1.Text;
+            Properties.Settings.Default.romPath = textBox2.Text;
+            Properties.Settings.Default.randomizerRomPath = textBox3.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        public void RestoreSavedValues()
+        {
+            emuPath = Properties.Settings.Default.emuPath;
+            textBox1.Text = emuPath;
+
+
+            romPath = Properties.Settings.Default.romPath;
+            textBox2.Text = romPath;
+
+            randomizerRomPath = Properties.Settings.Default.randomizerRomPath;
+            textBox3.Text = randomizerRomPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Select Emulator Path
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    foreach (string file in files)
+                    {
+                        if (Path.GetFileNameWithoutExtension(file) == "citra-qt")
+                        {
+                            emuPath = file;
+                            textBox1.Text = emuPath;
+                            SaveValues();
+                            return;
+                        }
+                    }
+
+                    MessageBox.Show("citra-qt.exe was not found in that folder, cancelling!");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Select OoT3D ROM Path
+
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "3DS Rom files (*.3ds, *.cia, *.cxi) | *.3ds; *.cia; *.cxi";
+
+                DialogResult result = ofd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(ofd.FileName))
+                {
+                    romPath = ofd.FileName;
+                    textBox2.Text = romPath;
+                    SaveValues();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Select OoT3D Randomizer Rom Path
+
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "3dsx files (*.3dsx) | *.3dsx";
+
+                DialogResult result = ofd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(ofd.FileName))
+                {
+                    randomizerRomPath = ofd.FileName;
+                    textBox3.Text = randomizerRomPath;
+                    SaveValues();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string emuFolder = Path.GetDirectoryName(emuPath);
+            Launcher.OpenRandomizer(emuPath, randomizerRomPath, Path.Combine(emuFolder, filesPath), Path.Combine(emuFolder, modPath));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Launcher.OpenGame(emuPath, romPath);
+        }
     }
 }
---
+++ OoT3D-Rando-Launcher/Launcher.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace OoT3D_Rando_Launcher
+{
+    class Launcher
+    {
+        private static Process ProcessStart(string processName, string romFile)
+        {
+            ProcessStartInfo pInfo = new ProcessStartInfo();
+            pInfo.FileName = processName;
+            pInfo.Arguments = romFile;
+
+            return Process.Start(pInfo);
+        }
+
+        public static void OpenRandomizer(string citraPath, string romFile, string fromPath, string toPath)
+        {
+            MessageBox.Show("Please make sure you do the following steps:\n\n1. Set the parameters the way you want.\n2. Select the \"Generate Randomizer\" option.\n3. Select the \"Citra Emulator\" option.\n4. Close Citra.");
+            ProcessStart(citraPath, romFile).WaitForExit();
+            MoveFiles(fromPath, toPath);
+        }
+
+        private static void MoveFiles(string fromPath, string toPath)
+        {
+            if (!Directory.Exists(fromPath))
+            {
+                MessageBox.Show("It looks like the seed wasn't generated properly...\n\nMake sure you selected the \"Citra Emulator\" option at the end!");
+                return;
+            }
+
+            if (!Directory.Exists(toPath))
+            {
+                Directory.CreateDirectory(toPath);
+            }
+
+            foreach (string file in Directory.GetFiles(fromPath))
+            {
+                File.Move(file, toPath + $@"\{Path.GetFileName(file)}", true);
+            }
+        }
+
+        public static void OpenGame(string citraPath, string romFile)
+        {
+            ProcessStart(citraPath, romFile);
+        }
+    }
+}
